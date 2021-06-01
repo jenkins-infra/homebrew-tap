d1ce71049b24a55e58375a58bea39a25ef3e4f50
jenkins-infra <gareth@bryncynfelin.co.uk>
2021-06-01 08:19:04 +0000

    Brew formula update for uc version 0.1.2

--- Formula/uc.rb
+++ Formula/uc.rb
@@ -5,24 +5,29 @@
 class Uc < Formula
   desc "Utility to update jenkins plugin.txt"
   homepage ""
-  version "0.1.1"
+  version "0.1.2"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jenkins-infra/uc/releases/download/0.1.1/uc-darwin-amd64.tar.gz"
-    sha256 "4d5c69737b27cacd44c72a07e396eb5a819f6ad4f1df0fa976c41152dd7e139d"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-darwin-amd64.tar.gz"
+      sha256 "725fe826d82b9ffa62de580ee4cb8822cf8f5a7d1030e6a937c544a9c9956aab"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-darwin-arm64.tar.gz"
+      sha256 "7ea2c251c865c0abd76b869b9e20dc7a3c6449c262c6f5a28bcb0b3b8849cbbc"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jenkins-infra/uc/releases/download/0.1.1/uc-darwin-arm64.tar.gz"
-    sha256 "cc6803d35ae48f3d844eebc9363f67a12c3a4614e853df5e205679d160b03f4b"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jenkins-infra/uc/releases/download/0.1.1/uc-linux-amd64.tar.gz"
-    sha256 "310827acfb8552bfe8cda0b1a95b5359b7adfb240f9d75e7057f4dce54c1e82c"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jenkins-infra/uc/releases/download/0.1.1/uc-linux-arm64.tar.gz"
-    sha256 "38e2f050709a5d4dd0061dcd5c3570fea969ed6f21fbf09c6ea25e2f1c2a5514"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-linux-amd64.tar.gz"
+      sha256 "cb9ec0c86b3c2cf87b121350b40a2b64650cc68dec9754e3eb7c537d3e8a6f69"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-linux-arm64.tar.gz"
+      sha256 "58eef7fc0cb646488aeddccaad65947ee1d217101b2ae3d969ab4e9d06d1673e"
+    end
   end
 
   def install
