e3ca8daa1df480bc89a6f6573e39b843f69f7088
jenkins-infra <gareth@bryncynfelin.co.uk>
2021-10-19 08:10:17 +0000

    Brew formula update for uc version 0.1.3

--- Formula/uc.rb
+++ Formula/uc.rb
@@ -5,28 +5,28 @@
 class Uc < Formula
   desc "Utility to update jenkins plugin.txt"
   homepage ""
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-darwin-amd64.tar.gz"
-      sha256 "725fe826d82b9ffa62de580ee4cb8822cf8f5a7d1030e6a937c544a9c9956aab"
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.3/uc-darwin-amd64.tar.gz"
+      sha256 "473ba5d706359b518bd66ca919d36c0a7155d50933643263919225411e3731bf"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-darwin-arm64.tar.gz"
-      sha256 "7ea2c251c865c0abd76b869b9e20dc7a3c6449c262c6f5a28bcb0b3b8849cbbc"
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.3/uc-darwin-arm64.tar.gz"
+      sha256 "637d6ae8fc17c21327f44be93f8114691677310955b90463712eba0ab02eeabd"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-linux-amd64.tar.gz"
-      sha256 "cb9ec0c86b3c2cf87b121350b40a2b64650cc68dec9754e3eb7c537d3e8a6f69"
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.3/uc-linux-amd64.tar.gz"
+      sha256 "1cb11b7e76dbc6c271d2fca659a6c6d6fc8e126ded78dc1edd175027e4099b5c"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jenkins-infra/uc/releases/download/0.1.2/uc-linux-arm64.tar.gz"
-      sha256 "58eef7fc0cb646488aeddccaad65947ee1d217101b2ae3d969ab4e9d06d1673e"
+      url "https://github.com/jenkins-infra/uc/releases/download/0.1.3/uc-linux-arm64.tar.gz"
+      sha256 "ae92bd665dc3cc2e9ca0718707052e35e4f58d3155fd740744a478269303473e"
     end
   end
 
