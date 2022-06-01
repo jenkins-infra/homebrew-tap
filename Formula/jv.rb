75bc3ba5e343803445f83c2d2ce319ac985816ee
jenkins-infra <gareth@bryncynfelin.co.uk>
2022-06-01 12:51:00 +0000

    Brew formula update for jenkins-version version 0.2.1

--- Formula/jv.rb
+++ Formula/jv.rb
@@ -8,17 +8,17 @@ class Jv < Formula
   version "0.2.1"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/jenkins-infra/jenkins-version/releases/download/0.2.1/jenkins-version-darwin-arm64.tar.gz"
-      sha256 "8de152087b851e30db3815b40b59adb24e22644dec24ebfa0f85ab4acb0dde41"
+    if Hardware::CPU.intel?
+      url "https://github.com/jenkins-infra/jenkins-version/releases/download/0.2.1/jenkins-version-darwin-amd64.tar.gz"
+      sha256 "0a0cd4c36fa2f6de0ec1bfa8eb5c13923ea72b91b7907a0fe5667fce32c8de0a"
 
       def install
         bin.install "jv"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/jenkins-infra/jenkins-version/releases/download/0.2.1/jenkins-version-darwin-amd64.tar.gz"
-      sha256 "d65d6139af851f531f5a3ab8b55b44d1935fa2bfc4f75523c80d2c4b3f7cd298"
+    if Hardware::CPU.arm?
+      url "https://github.com/jenkins-infra/jenkins-version/releases/download/0.2.1/jenkins-version-darwin-arm64.tar.gz"
+      sha256 "9e6c6d20b0ad78f6b457a729e5671258e7f1229b0bc653ba78e58227f1e51832"
 
       def install
         bin.install "jv"
@@ -29,7 +29,7 @@ class Jv < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/jenkins-infra/jenkins-version/releases/download/0.2.1/jenkins-version-linux-arm64.tar.gz"
-      sha256 "2c50b7f48d2dae13c0733f8b8278d2a7de2860c0c7d4e26213f39fd763b42647"
+      sha256 "4e404175a5ab32e11b1ab56cccadad1b5c28153813d05fa3d8f8ca0a0639da1d"
 
       def install
         bin.install "jv"
@@ -37,7 +37,7 @@ class Jv < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/jenkins-infra/jenkins-version/releases/download/0.2.1/jenkins-version-linux-amd64.tar.gz"
-      sha256 "e55354b914927cd29565297fa67184a0c80a460594fb808b276675ab828432ea"
+      sha256 "87db615d69cc0ccaf9339be7350b8a18abcd7763848f5f06160eabc3af7135a3"
 
       def install
         bin.install "jv"
