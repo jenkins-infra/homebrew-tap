19cbfaf3c8e1a478aef5531a040369beb4bce0cf
Valentin Delaye <jonesbusy@users.noreply.github.com>
2024-12-10 16:00:54 +0100

    Add comment about version scheme
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- Formula/plugin-modernizer.rb
+++ Formula/plugin-modernizer.rb
@@ -1,5 +1,7 @@
 class PluginModernizer < Formula
     desc "Plugin Modernizer"
+    # Note: Brew don't really like our versions scheme for CD. Implicitly it consider "499.vb_86f97f0b_197" 
+    # as version "197" which is incorrect. So using version which only "%d" part for CD
     version "911.v211a_1d5a_7a_a_f".split(".")[0]
     homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
     url "https://github.com/jenkins-infra/plugin-modernizer-tool/releases/download/911.v211a_1d5a_7a_a_f/jenkins-plugin-modernizer-911.v211a_1d5a_7a_a_f.jar"
