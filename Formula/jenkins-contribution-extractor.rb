0ea4483c3c308bddd0fda3cd710d702016ab1d1f
goreleaserbot <jenkins-infra-team@googlegroups.com>
2024-12-19 08:49:17 +0000

    Brew formula update for jenkins-contribution-extractor version v0.2.19

--- Formula/jenkins-contribution-extractor.rb
+++ Formula/jenkins-contribution-extractor.rb
@@ -5,21 +5,21 @@
 class JenkinsContributionExtractor < Formula
   desc "Jenkins Contribution data extractor and analyzer."
   homepage "https://github.com/jenkins-infra/jenkins-contribution-extractor"
-  version "0.2.18"
+  version "0.2.19"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.18/jenkins-contribution-extractor_0.2.18_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "c4557cc3b68216e660a318d6baabbf9a65dcbc70bb59310cb7b30a6ff4e8af12"
+      url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.19/jenkins-contribution-extractor_0.2.19_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "c34bdb36d1f1286c2a22319432a86aca015ad9d686d13adc139f27c819adfb78"
 
       def install
         bin.install "jenkins-contribution-extractor"
       end
     end
     on_arm do
-      url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.18/jenkins-contribution-extractor_0.2.18_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "7bc245d8b2df49b45c7138fb0ab51104841cf5c8b6e4915ad662cbebcc29361c"
+      url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.19/jenkins-contribution-extractor_0.2.19_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "ce60978b2052990d1524f040b21421cde97e9adec844fbd8b3292454ff7da90a"
 
       def install
         bin.install "jenkins-contribution-extractor"
@@ -30,8 +30,8 @@ class JenkinsContributionExtractor < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.18/jenkins-contribution-extractor_0.2.18_linux_amd64.tar.gz", using: CurlDownloadStrategy
-        sha256 "bf3a38597672815c926efb6c4b63240dfe51a526b49354292c97a97364d99c98"
+        url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.19/jenkins-contribution-extractor_0.2.19_linux_amd64.tar.gz", using: CurlDownloadStrategy
+        sha256 "7299ea8891501fd0f731409ed84a29b3ab4af8afc71faf6540e1536016457cf7"
 
         def install
           bin.install "jenkins-contribution-extractor"
@@ -40,8 +40,8 @@ class JenkinsContributionExtractor < Formula
     end
     on_arm do
       if !Hardware::CPU.is_64_bit?
-        url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.18/jenkins-contribution-extractor_0.2.18_linux_armv6.tar.gz", using: CurlDownloadStrategy
-        sha256 "f92c925eefd490abeb011133f7bd969538a1c12dbbe3836ae7049b6decd84ac4"
+        url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.19/jenkins-contribution-extractor_0.2.19_linux_armv6.tar.gz", using: CurlDownloadStrategy
+        sha256 "81a163a806188521fd293c60bdfb86e777eee0e5b15470a77281598ce4b2da7c"
 
         def install
           bin.install "jenkins-contribution-extractor"
@@ -50,8 +50,8 @@ class JenkinsContributionExtractor < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.18/jenkins-contribution-extractor_0.2.18_linux_arm64.tar.gz", using: CurlDownloadStrategy
-        sha256 "b6f839bec6ed449739c796e97dc2c0808e8d9a9976d03abc66e4559aefe76b1e"
+        url "https://github.com/jenkins-infra/jenkins-contribution-extractor/releases/download/v0.2.19/jenkins-contribution-extractor_0.2.19_linux_arm64.tar.gz", using: CurlDownloadStrategy
+        sha256 "4d8337f912febf17d86272e4f3400d79598bfddf468b13faa4ddb6c578588f76"
 
         def install
           bin.install "jenkins-contribution-extractor"
