46608b61da934443c40ad9370d4a29948c6cca18
jenkins-infra <jenkins-infra-team@googlegroups.com>
2025-08-18 16:28:24 +0000

    plugin-modernizer 2157.v0570b_b_6144c0

--- Formula/plugin-modernizer.rb
+++ Formula/plugin-modernizer.rb
@@ -2,15 +2,15 @@ class PluginModernizer < Formula
     desc "Plugin Modernizer"
     # Note: Brew don't really like our versions scheme for CD. Implicitly it consider 499.vb_86f97f0b_197 as version 197 which is incorrect
     # So using version which  only first numeric part for CD
-    version "2148.v7e1b_1894465a_".split(".")[0]
+    version "2157.v0570b_b_6144c0".split(".")[0]
     homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
-    url "https://github.com/jenkins-infra/plugin-modernizer-tool/releases/download/2148.v7e1b_1894465a_/jenkins-plugin-modernizer-2148.v7e1b_1894465a_.jar"
-    sha256 "78995d48a75a1434d1bb28c8eb8b7187d52e229c93b958c793373e575153db88"
+    url "https://github.com/jenkins-infra/plugin-modernizer-tool/releases/download/2157.v0570b_b_6144c0/jenkins-plugin-modernizer-2157.v0570b_b_6144c0.jar"
+    sha256 "da837fd898f2d2f5b90c94b6f49f22e5a7421b3c2c99c3304f82d07cbc4f557b"
     license "MIT"
 
     def install
-      libexec.install "jenkins-plugin-modernizer-2148.v7e1b_1894465a_.jar"
-      bin.write_jar_script libexec/"jenkins-plugin-modernizer-2148.v7e1b_1894465a_.jar", "plugin-modernizer"
+      libexec.install "jenkins-plugin-modernizer-2157.v0570b_b_6144c0.jar"
+      bin.write_jar_script libexec/"jenkins-plugin-modernizer-2157.v0570b_b_6144c0.jar", "plugin-modernizer"
     end
 
     test do
